ed5adf2423f7fc3c5bfcea3f05cfbb8ae1ffe95d
Michael Gillam <michaelgillam89@gmail.com>
2022-02-17 18:13:57 -0600

    Wrapped up front end challenge

---
+++ frontend-challenge/RUNBOOK.md
@@ -0,0 +1,12 @@
+## Sources
+
+- [Next.js](https://nextjs.org/)
+- [React](https://reactjs.org/)
+- [Chakra-UI](https://chakra-ui.com/)
+- [Formik](https://formik.org/)
+
+## How to run the project
+
+- Make sure you have node and npm installed on your machine
+- Open a terminal inside the **frontend-challenge** folder and run `npm install`. This will install the all dependencies from **package.json**.
+- While inside the **frontend-challenge** folder, run `npm run dev`. The project will open at this url: http://localhost:3000/
--- frontend-challenge/components/layouts/Footer.tsx
+++ frontend-challenge/components/layouts/Footer.tsx
@@ -1,15 +1,34 @@
-import { Box, Container, Link, SimpleGrid, Stack, Text, useColorModeValue } from '@chakra-ui/react'
-import moment from 'moment'
+import {
+  Box,
+  Container,
+  Flex,
+  Heading,
+  SimpleGrid,
+  Stack,
+  Text,
+  useColorModeValue
+} from '@chakra-ui/react'
 import React from 'react'
 
 export default function Footer() {
   const bg = useColorModeValue('gray.200', 'gray.800')
 
+  const renderContactBlock = (title, content) => (
+    <Flex flexDir="column" justifyContent="center" alignItems="center">
+      <Heading size="md" mb={2} fontWeight="bold">
+        {title}
+      </Heading>
+      <Text fontSize="sm">{content}</Text>
+    </Flex>
+  )
+
   return (
     <Box as="footer" alignItems="center" p={6} bg={bg}>
-      <Container as={Stack} maxW="container.lg" py={10}>
-        <SimpleGrid columns={{ base: 1, sm: 2, md: 4 }} spacing={8}>
-          dsfsdafdsafdas
+      <Container as={Stack} maxW="container.lg" py={5}>
+        <SimpleGrid columns={{ base: 1, md: 3 }} spacing={8}>
+          {renderContactBlock('EMAIL ADDRESS', 'hello@greatsite.com')}
+          {renderContactBlock('MAILING ADDRESS', '123 Anywhere St. Any City, ST 12345')}
+          {renderContactBlock('PHONE NUMBER', '(123) 456-7890')}
         </SimpleGrid>
       </Container>
     </Box>
