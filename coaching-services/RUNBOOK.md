fd5c58df82d8a306639d84f28181ec219b2c0da8
HT <hankthornlee@hotmail.com>
2022-07-11 20:06:03 -0700

    Front End Challenge Code
    - Added a company title
    - Added reusable basic table
    - Added company logo favicon
    - Added contact us form
    - Added basic validation
    - Dump table to console when valid data submitted

---
+++ coaching-services/RUNBOOK.md
@@ -0,0 +1,15 @@
+## Running Application
+- Ensure that NPM and Node.js is installed
+- inside of coaching-services folder where this file is located run the following:
+    - npm i
+    - npm start
+
+
+## Technology Utilized
+- React
+- PrimeReact Component Library
+- PrimeFlex CSS Styling Library
+
+
+## Notes
+- npx create-react-app was used to scaffold the application for rapid development
---
+++ coaching-services/src/App.js
@@ -0,0 +1,67 @@
+import logo from './logo.svg';
+import './App.css';
+import '/node_modules/primeflex/primeflex.css'
+import 'primeicons/primeicons.css';
+import Table from './components/DataTable/Table'
+import ContactForm from './components/ContactForm/ContactForm'
+import { Card } from 'primereact/card';
+import { Button } from 'primereact/button';
+import '/node_modules/primereact/resources/themes/vela-blue/theme.css'
+
+
+function App() {
+
+  const headers ={
+    name: 'Name',
+    field: 'Field',
+    years: 'Years of Experience',
+    colleges: 'Higher Education'
+  }
+
+  const educatorList = [
+    {
+      name: 'Charles Jones',
+      field: 'Computer Science',
+      years: 30,
+      colleges: 'Stanford'
+    },
+    {
+      name: 'Neil Armstrong',
+      field: 'Space',
+      years: 35,
+      colleges: 'Purdue'
+    },
+    {
+      name: 'Lefty McLefterson',
+      field: 'Left Handed Items',
+      years: 50,
+      colleges: 'Texas A&M'
+    },
+    {
+      name: 'Bob Dale',
+      field: 'Mirco Biology',
+      years: 33,
+      colleges: 'Carnegie Mellon'
+    }
+  ]
+
+
+  return (
+    <div className="App ">
+      <Card>
+        <header>
+            <h1>CoachPro Stream</h1>
+        </header>
+      </Card>
+      
+    
+      <Table className='' data={educatorList} headers={headers} />
+      
+      <ContactForm tableData={educatorList} headers={headers}/>
+    
+     
+    </div>
+  );
+}
+
+export default App;
