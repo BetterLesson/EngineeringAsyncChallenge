37cc7b7a854b77a2ca7759f61af1ac2f73918e48
HT <hankthornlee@hotmail.com>
2022-07-12 00:29:16 -0700

    Part 3 Section B Simuation
    Simulated all inserts and relationship updates within the database for a dataset for the diagram in the same folder

---
+++ nosqlschema/RUNBOOK.md
@@ -0,0 +1,6 @@
+## Running Application
+- Ensure Node.js is installed
+- inside of nosqlschema folder where this file is located run the following:
+    - node simulate-db-conversion.js
+
+
--- nosqlschema/simulate-db-conversion.js
+++ nosqlschema/simulate-db-conversion.js
@@ -1,16 +1,55 @@
-// |Field Name | Type   | Sample Value |
-// | --------- | ------ | ------------ |
-// |UUID  | Number | 12345678         |
-// |Customer Name | String | Jane Doe    |
-// |Cell Phone | String | 405.867.5309 |
-// |Email      | String |jane_teacher@gmail.com |
-// |Address    | String | 123 School Way, Dallas TX 75001 |
-// |Coaching Service ID | List of Number | [37,2002,101] |
-// |Book Set ID| List of Number | [22,21] |
-// |Order Total | Number | 37.99 |
-// |Order Date | String | 12/25/22T23:00:05Z |
-// |Discount Code | String | CHEAP |
+const dataStore = require('./dataStore.json')
 
-// **Part 2**
+const books = dataStore.books
+const bookSets = dataStore.book_sets
+const coachingServices = dataStore.coaching_services
+const customers = dataStore.customers
 
-//   The next step is to create a solution that can migrate the data from their current database to your new relational database. Write a basic program in a language of your choice such as Python or Java. Read in a JSON file that contains the records from the old database, format the data to match your new data model, and print SQL statements to console/standard IO that would insert these records into the new database.
+
+//Add Books first
+for(book of books){
+    console.log(`INSERT into BOOK:`, book)
+}
+console.log(``)
+for(bookSet of bookSets){
+    console.log(`INSERT into BOOK_SET:`, bookSet)
+    for(id of bookSet.books){
+        console.log(`INSERT into Books_BookSets: book_ids=${id}, book_set_id=${bookSet.UUID}`)
+    }
+}
+for(customer of customers){
+    //Add Coaching Services
+    console.log(`INSERT into Customer: `,customer)
+    
+    coachingServices.filter((service)=>{
+        if (customer.coaching_service_ids.includes(service.UUID)){
+            console.log(`INSERT into Coaching_Services:`, service)
+            return true
+        }
+    })
+
+    //Add Coaching Relationships
+    for(service_id of customer.coaching_service_ids){
+        console.log(`INSERT into Customers_Coaching_Services: customer_id=${customer.UUID} coaching_service_id=${service_id}`)
+    }
+
+    //Add Book Set Realationships
+    for(id of customer.book_set_ids){
+        console.log(`INSERT into Customers_BookSets: customer_id=${customer.UUID} coaching_service_id=${id}`)
+    }
+    // "order_total": 37.99,
+    // "order_date": "12-25-22T23:00:05Z",
+    // "discount_code": "CHEAP"
+    //Add Orders
+    const order = {id:'1',total:customer.order_total,date:customer.order_date,discout_code:customer.discout_code}
+    console.log(`INSERT into Orders UID AUTO generated on INSERT:`, order)
+
+    //Add Orders Relationships
+    for(service_id of customer.coaching_service_ids){
+        console.log(`INSERT into Orders_Coaching_Services: order_id=${order.id} coaching_service_id=${service_id}`)
+    }
+
+    for(id of customer.book_set_ids){
+        console.log(`INSERT into Orders_BookSets: order_id=${order.id} book_set_id=${id}`)
+    }
+}
