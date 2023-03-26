580dd2bf2a5bc3d0bb11cf230f2de6351d8eb1e5
Karl Cereno <kagc.dev@gmail.com>
2023-03-26 19:42:10 -0400

    Create dummy events

--- server.js
+++ server.js
@@ -12,7 +12,33 @@ app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
 
-const data = [];
+const reservations = [
+  {
+    name: 'Birthday party',
+    startTime: new Date('2023-03-25T15:00:00Z'),
+    endTime: new Date('2023-03-25T16:00:00Z'),
+  },
+  {
+    name: 'Concert',
+    startTime: new Date('2023-03-25T17:30:00Z'),
+    endTime: new Date('2023-03-25T18:30:00Z'),
+  },
+  {
+    name: 'Business meeting',
+    startTime: new Date('2023-03-26T09:00:00Z'),
+    endTime: new Date('2023-03-26T10:00:00Z'),
+  },
+  {
+    name: 'Movie screening',
+    startTime: new Date('2023-03-26T12:30:00Z'),
+    endTime: new Date('2023-03-26T14:00:00Z'),
+  },
+  {
+    name: 'Networking event',
+    startTime: new Date('2023-03-24T13:00:00Z'),
+    endTime: new Date('2023-03-24T15:00:00Z'),
+  },
+];
 
 // Endpoints
 app.get('/', (req, res) => {
