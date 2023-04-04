c3cc231e92970262579ae82386a69e98d1aadd61
nicmlu <NicoleMLucien@gmail.com>
2023-04-04 12:00:27 -0400

    add contact form code

--- frontend/src/components/ContactForm/ContactForm.js
+++ frontend/src/components/ContactForm/ContactForm.js
@@ -5,7 +5,76 @@ function ContactForm() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [selectedOption, setSelectedOption] = useState("");
-  return <div>Placeholder code</div>;
+
+  return (
+    <div className="form-container">
+      <h1 className="form-headline">Join our mailing list</h1>
+
+      <div className="form">
+        <div className="form-left">
+          <div>
+            <form onSubmit={handleSubmit} id="contact-form">
+              <div>
+                <label htmlFor="name">Full Name</label>
+                <div>
+                  <input
+                    type="text"
+                    id="name"
+                    value={name}
+                    onChange={handleNameChange}
+                  />
+                </div>
+              </div>
+              <div>
+                {" "}
+                <label htmlFor="email">Email</label>
+                <div>
+                  <input
+                    type="email"
+                    id="email"
+                    value={email}
+                    onChange={handleEmailChange}
+                  />
+                </div>
+              </div>
+              <div>
+                <label htmlFor="dropdown">Industry:</label>
+                <div>
+                  <select
+                    id="dropdown"
+                    name="dropdown"
+                    value={selectedOption}
+                    onChange={handleDropdownChange}
+                  >
+                    <option value="E-Sports">E-Sports</option>
+                    <option value="Sports/Fitness">Sports/Fitness</option>
+                    <option value="Professional Services">
+                      Professional Services
+                    </option>
+                  </select>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+        <div className="form-right">
+          <div className="form-text">
+            Join our mailing to receive notifications about program availability
+            and special discounts
+          </div>
+          <div>
+            <button
+              type="submit"
+              form="contact-form"
+              className="contact-button"
+            >
+              Sign Up
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default ContactForm;
