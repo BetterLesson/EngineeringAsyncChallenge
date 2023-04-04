1b870d0b32e48db818b499877cd9a0f654b77f1e
nicmlu <NicoleMLucien@gmail.com>
2023-04-04 10:07:06 -0400

    update Company Title code including register now button

--- frontend/src/components/CompanyTitle/CompanyTitle.js
+++ frontend/src/components/CompanyTitle/CompanyTitle.js
@@ -2,7 +2,37 @@ import React from "react";
 import "./CompanyTitle.css";
 
 function CompanyTitle() {
-  return <div>Placeholder Div</div>;
+  return (
+    <div className="company-title-container">
+      <div className="company-title">
+        <div>
+          <img
+            src="/favicon.ico"
+            alt="Company Logo"
+            className="company-title__logo"
+          />
+        </div>
+        <div className="company-title__text">
+          <h1>Better Lesson</h1>
+          <h1>Professional Coaching</h1>
+        </div>
+
+        <div className="company-title__tagline">
+          <h2>PROFESSIONAL COACHING SEMINARS & MENTORSHIP</h2>
+        </div>
+        <div>
+          <button
+            onClick={handleClick}
+            className="register-button"
+            disabled={false}
+          >
+            {" "}
+            Register Now{" "}
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default CompanyTitle;
