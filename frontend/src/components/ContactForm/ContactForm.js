083ea8201eda426b14347ab91ac193a5f5e1edf6
nicmlu <NicoleMLucien@gmail.com>
2023-04-04 11:48:41 -0400

    set initial state for contact form

--- frontend/src/components/ContactForm/ContactForm.js
+++ frontend/src/components/ContactForm/ContactForm.js
@@ -2,6 +2,9 @@ import React, { useState } from "react";
 import "./ContactForm.css";
 
 function ContactForm() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [selectedOption, setSelectedOption] = useState("");
   return <div>Placeholder code</div>;
 }
 
