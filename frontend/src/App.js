2806a3f8a522965ee038e2ce7675f7a6225a142b
nicmlu <NicoleMLucien@gmail.com>
2023-04-04 12:04:42 -0400

    import contact form in App file and render

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,14 @@
 // import './App.css';
 import CompanyTitle from "./components/CompanyTitle/CompanyTitle";
 import CoachingExpertsTable from "./components/CoachingExperts/CoachingExperts";
+import ContactForm from "./components/ContactForm/ContactForm";
 
 function App() {
   return (
     <div className="App">
       <CompanyTitle />
       <CoachingExpertsTable />
+      <ContactForm />
     </div>
   );
 }
